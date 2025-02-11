73642bfc766855d29d920c64dae3a4ff6682e909
zetsuboii <hmzkrb@gmail.com>
2025-02-11 14:07:17 -0800

    feat: add prompting

---
+++ src/prompt.py
@@ -0,0 +1,47 @@
+from vecdb import VecDB
+from langchain.chat_models import init_chat_model
+from langchain_core.messages import SystemMessage, HumanMessage
+
+llm = init_chat_model(model="gpt-4o", model_provider="openai")
+
+
+def generate_response(query, vecdb: VecDB):
+    retrieved_docs = vecdb.search(query)
+    context = "\n\n".join(
+        f"{doc['path']}: {doc['text']} (score: {doc['score']})"
+        for doc in retrieved_docs
+    )
+    print("=== Context ===")
+    print(context)
+    print("=== Query ===")
+    print(query)
+    print("=== Response ===")
+
+    system_message = SystemMessage(
+        content=f"""
+        You are an assistant that helps answer questions using the following notes:
+
+        {context}
+
+        Answer the user's query.
+        Reference the notes by their path whenever applicable. Reference them like
+        [1], [2] and have a "References" section at the end that matches the reference
+        paths.
+        
+        For example, if there's a reference to /Vault/RustNotes.md in a sentence about
+        Rust, it should look like:
+        
+        ```md
+        Rust is a systems programming language that is fast, safe, and easy to use.[1]
+
+        References:
+        - [1]: /Vault/RustNotes.md
+        ```
+        
+        """
+    )
+    user_message = HumanMessage(content=query)
+    messages = [system_message, user_message]
+
+    for token in llm.stream(messages):
+        print(token.content, end="", flush=True)
